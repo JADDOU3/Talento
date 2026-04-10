203fefee7ad354653b73886f9f11db75261b877c
JADDOU3 <135611544+JADDOU3@users.noreply.github.com>
2026-04-10 14:00:57 +0300

    Revert "Merge pull request #5 from JADDOU3/RC2-Samer/ChildEntity(orsomethingidk)"
    This reverts commit 398d6aaaba3c8d0ad8137630f58996f074783ffb, reversing
    changes made to 53edd67bd9bba473c5f1d87802eebf4099740a67.
    
    i
    esc
    :wq

--- Backend/src/main/java/org/example/backend/Dto/RegisterDto.java
+++ Backend/src/main/java/org/example/backend/Dto/RegisterDto.java
@@ -1,11 +1,8 @@
 package org.example.backend.Dto;
 
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.example.backend.util.Gender;
 
 @Data
 @AllArgsConstructor
@@ -13,7 +10,6 @@ import org.example.backend.util.Gender;
 public class RegisterDto {
     private String name;
     private String email;
-    @Enumerated(EnumType.STRING)
-    private Gender gender;// *** Changed datatype from String to Gender and  name from Gender to gender, delete comment after review ^_^ ***
+    private String Gender;
     private String password;
 }
