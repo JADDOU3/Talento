c9a7a403e0b8b7857a5870bcfbdf28c3ae7e9714
JADDOU3 <135611544+JADDOU3@users.noreply.github.com>
2026-04-14 22:01:30 +0300

    deploy configurations

--- Backend/src/main/java/org/example/backend/config/SecurityConfig.java
+++ Backend/src/main/java/org/example/backend/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return  http.csrf(customizer -> customizer.disable())
                     .authorizeHttpRequests(request ->
-                            request.requestMatchers("/login" , "/register").permitAll()
+                            request.requestMatchers("/api/login" , "/api/register").permitAll()
                                     .anyRequest().authenticated())
                     .httpBasic(Customizer.withDefaults())
                     .sessionManagement(session ->
