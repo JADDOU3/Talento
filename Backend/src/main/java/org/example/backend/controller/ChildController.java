5ab8ce8a3177427dd6ade13f61e180801c3019c6
Talento <sameralhindi41@gmail.com>
2026-04-18 14:06:20 +0300

    ChildController

--- Backend/src/main/java/org/example/backend/controller/ChildController.java
+++ Backend/src/main/java/org/example/backend/controller/ChildController.java
@@ -39,7 +39,7 @@ public class ChildController {
     @PostMapping("")
     public ResponseEntity<Child> addChild(@RequestBody ChildDto d){
         return new ResponseEntity<>(childService.createChild(d), HttpStatus.CREATED);
-    }//TESTETSTEST
+    }
 
     @PutMapping("/{id}")
     public ResponseEntity<Child> updateChild(@PathVariable int id, @RequestBody Child x){
