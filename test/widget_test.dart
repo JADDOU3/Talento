f82ed61dbea534b529bfb12d12a461baf9afa817
JADDOU3 <22the7@gmail.com>
2026-05-08 17:37:15 +0300

    Revert "Merge pull request #55 from JADDOU3/flutter-web"
    This reverts commit 284ee79ddd68c62d39b5dd7956c80d2fb2a4350e, reversing
    changes made to e5e2b01fb3647d45c086f06f5e6b73459e358077.

--- test/widget_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:talento_app/main.dart';
-
-void main() {
-  testWidgets('Talento app smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const TalentoApp());
-  });
-}
