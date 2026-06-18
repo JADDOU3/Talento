7e5b835d08cbc9014799e47efd77a87a09fb77dd
NadiaBliableh <nadiabliableh@gmail.com>
2026-06-18 03:48:52 +0300

    feat: add Emotion Chain Analyzer activity integration

--- Frontend/Mobile/lib/screens/roadmap/roadmap_screen.dart
+++ Frontend/Mobile/lib/screens/roadmap/roadmap_screen.dart
@@ -13,6 +13,7 @@ import 'widgets/roadmap_game_board.dart';
 import 'widgets/roadmap_header.dart';
 import 'widgets/roadmap_state_views.dart';
 import '../../activities/pattern_hacker/pattern_hacker_intro.dart';
+import '../../activities/emotion_chain/emotion_chain_intro.dart';
 
 class RoadmapScreen extends StatelessWidget {
   final int kitId;
@@ -186,6 +187,21 @@ class _RoadmapView extends StatelessWidget {
 
       return;
     }
+    if (activityName == 'emotion chain analyzer') {
+      Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (_) => EmotionChainIntro(
+          childId: childId,
+          kitId: kitId,
+          activityId: activity.activityId,
+        ),
+      ),
+    ).then((_) {
+      _refreshRoadmapIfMounted(context);
+    });
+  return;
+}
 
     _showMessage(
       context,
