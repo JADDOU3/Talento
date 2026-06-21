290b3a2b8b750d5a987a654a56a348a3c88fa384
HamzaAbdulSalam <hamzasalam554@gmail.com>
2026-06-21 17:32:51 +0300

    Tower builder part1

--- Frontend/Mobile/lib/screens/roadmap/roadmap_screen.dart
+++ Frontend/Mobile/lib/screens/roadmap/roadmap_screen.dart
@@ -5,6 +5,7 @@ import '../../activities/color_lab/color_lab_launcher.dart';
 import '../../activities/conflict_resolution/conflict_resolution_intro.dart';
 import '../../activities/mirror_mind/mirror_mind_intro.dart';
 import '../../activities/pattern_hacker/pattern_hacker_intro.dart';
+import '../../activities/tower_builder/tower_builder_launcher.dart';
 import '../../core/theme/app_colors.dart';
 import '../../cubits/roadmap/roadmap_cubit.dart';
 import '../../cubits/roadmap/roadmap_state.dart';
@@ -152,6 +153,20 @@ class _RoadmapView extends StatelessWidget {
         context,
         MaterialPageRoute(
           builder: (_) => ColorLabLauncher(
+            activityId: activity.activityId,
+            kitId: kitId,
+            childId: childId,
+          ),
+        ),
+      ).then((_) => _refreshRoadmapIfMounted(context));
+      return;
+    }
+
+    if (activityName == 'tower builder') {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (_) => TowerBuilderLauncher(
             activityId: activity.activityId,
             kitId: kitId,
             childId: childId,
