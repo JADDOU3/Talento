a58ac0f3fc8adf23f5ee1fdc2fa5d22758e687e1
Fawzia AbuAsbah <s12219765@stu.najah.edu>
2026-07-08 02:57:37 +0300

    Fix Bodily Maze Levels

--- Frontend/Mobile/lib/activities/bodily_maze/bodily_maze_game_screen.dart
+++ Frontend/Mobile/lib/activities/bodily_maze/bodily_maze_game_screen.dart
@@ -21,6 +21,7 @@ class BodilyMazeGameScreen extends StatelessWidget {
   final int childId;
   final int sessionId;
   final int? startLevelId;
+  final int? startLevelNumber;
 
   const BodilyMazeGameScreen({
     super.key,
@@ -29,6 +30,7 @@ class BodilyMazeGameScreen extends StatelessWidget {
     required this.childId,
     required this.sessionId,
     this.startLevelId,
+    this.startLevelNumber,
   });
 
   @override
@@ -40,7 +42,10 @@ class BodilyMazeGameScreen extends StatelessWidget {
         activitySessionId: activitySessionId,
         childId: childId,
         sessionId: sessionId,
-      )..loadGame(startLevelId: startLevelId),
+      )..loadGame(
+        startLevelId: startLevelId,
+        startLevelNumber: startLevelNumber,
+      ),
       child: _BodilyMazeView(),
     );
   }
